1860fca50923709839143645a37efd0ec0e7616e
Mohammad <mohamadhosseini2121@gmail.com>
2020-09-25 00:05:22 +0330

    user profile layout creation

--- app/src/main/java/com/example/bargh/view/fragment/UserProfileFragment.java
+++ app/src/main/java/com/example/bargh/view/fragment/UserProfileFragment.java
@@ -1,19 +1,17 @@
 package com.example.bargh.view.fragment;
 
 import android.os.Bundle;
+
 import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.bargh.R;
 
+public class UserProfileFragment extends Fragment {
 
-public class PurchasesFragment extends Fragment {
-
-    public PurchasesFragment() {
-        // Required empty public constructor
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -21,5 +19,8 @@ public class PurchasesFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_user_profile, container, false);
     }
+
+
+
 
 }
